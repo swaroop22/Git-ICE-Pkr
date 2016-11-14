9e433e855d1c8759ce67a33d6c1de01f6a399534
Student <s525756@ch140007m.nwmissouri.edu>
2016-11-14 16:25:50 -0600

    Initial Commit

---
+++ Git-ICE-Pkr/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-ICE-Pkr
+//
+//  Created by Pothamsetti,Krishna Jyothi Swaroop Reddy on 11/14/16.
+//  Copyright © 2016 Pothamsetti,Krishna Jyothi Swaroop Reddy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
