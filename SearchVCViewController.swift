4638e7441e4203b7e9ab4da74c7f713c08e56c1c
Student <s525756@ch140007m.nwmissouri.edu>
2016-11-14 16:44:18 -0600

    I added searchvc renamed 1st view controller and second view controller

---
+++ SearchVCViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchVCViewController.swift
+//  Git-ICE-Pkr
+//
+//  Created by Pothamsetti,Krishna Jyothi Swaroop Reddy on 11/14/16.
+//  Copyright © 2016 Pothamsetti,Krishna Jyothi Swaroop Reddy. All rights reserved.
+//
+
+import UIKit
+
+class SearchVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
